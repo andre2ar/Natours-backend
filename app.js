fa8e6acb8212a7b4dc0928801b636d45c4615b97
André <andre2ar@outlook.com>
2020-01-17 15:36:51 -0300

    Import data script

--- app.js
+++ app.js
@@ -17,11 +17,6 @@ if(process.env.NODE_ENV === 'development') {
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-    console.log("Hello from middleware!");
-    next();
-});
-
-app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
