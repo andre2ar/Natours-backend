5f156a38f20d6a157b86347c2bf6e5505b345777
André <andre2ar@outlook.com>
2020-01-23 11:35:11 -0300

    Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -30,6 +30,21 @@ exports.getAllTours = async (req, res) => {
             query = query.select('-__v');
         }
 
+        // 4) PAGINATION
+        const page = Math.abs(req.query.page * 1) || 1;
+        const limit = req.query.limit * 1 || 100;
+
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if(req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) {
+                throw new Error('This page do not exists');
+            }
+        }
+
         const  tours = await query;
 
         res.status(200).json({
