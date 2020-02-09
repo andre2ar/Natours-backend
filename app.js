cf31b913c9e7ce9ab5b146d475edc2961b710ddb
Andre Alvim Ribeiro <andre2ar@outlook.com>
2020-02-09 20:17:48 -0300

    XSS and Sanitization

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -31,6 +33,12 @@ app.use('/api', limiter);
 app.use(express.json({
     limit: '10kb'
 }));
+
+// DATA SANITIZATION AGAINST NOSQL QUERY INJECTION
+app.use(mongoSanitize());
+
+// DATA SANITIZATION AGAINST XSS
+app.use(xss());
 
 /*Serve static files*/
 app.use(express.static(`${__dirname}/public`));
