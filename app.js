eb852ebc8d4c6bc2529a8fd833cecb3b341f340c
Andre Alvim Ribeiro <andre2ar@outlook.com>
2020-02-09 20:02:50 -0300

    API Limiter

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/AppError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,12 +9,18 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// Middlewares
-app.use(express.json());
-
+// GLOBAL Middlewares
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
 }
+const limiter = rateLimit({
+    max: 100,
+    windowMs:60 * 60 * 1000,
+    message: "Too many requests from this IP, please try again in an hour!"
+});
+app.use('/api', limiter);
+
+app.use(express.json());
 
 /*Serve static files*/
 app.use(express.static(`${__dirname}/public`));
