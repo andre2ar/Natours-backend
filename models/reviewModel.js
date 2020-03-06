a2acb00dc8418511a5f4e8d205eb5ea639c04a60
André <andre2ar@outlook.com>
2020-03-06 09:53:27 -0300

    Delete and Update reviews calculations

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -57,14 +57,33 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
         }
     ]);
 
-    await Tour.findByIdAndUpdate(tourId, {
-        ratingsQuantity: stats[0].nRatings,
-        ratingsAverage: stats[0].avgRating
-    })
+    if(stats.length > 0) {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: stats[0].nRatings,
+            ratingsAverage: stats[0].avgRating
+        });
+    } else {
+        await Tour.findByIdAndUpdate(tourId, {
+            ratingsQuantity: 0,
+            ratingsAverage: 4.5
+        });
+    }
 };
 
-reviewSchema.pre('save', function() {
-    this.constructor.calcAverageRatings(this.tour);
+reviewSchema.pre('save', async function() {
+    await this.constructor.calcAverageRatings(this.tour);
+});
+
+// findByIdAndUpdate
+// findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+    this.r = await this.findOne();
+    next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+    // await this.findOne(); does NOT work here, query already executed
+    await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 
 const Review = mongoose.model('Review', reviewSchema);
